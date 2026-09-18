d5272052286f5266012c5b104bfad6319a7ea694
dineshreddyakuletidineshreddy-sys <dineshreddyakuletidineshreddy@gmail.com>
2026-09-18 21:32:11 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "adr-study-helper-v2";
+
+const FILES_TO_CACHE = [
+    "SmartStudentHelper_Complete.html",
+    "manifest.json",
+    "icon.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
